7c974cdbf691e8daa3ba41095bb98820e1898492
manhongdao <0915809219a@gmail.com>
2022-03-09 10:06:10 +0700

    update constant

--- constants/index.ts
+++ constants/index.ts
@@ -1,2 +1,2 @@
-export const ANDROID_APP = 'https://apps.apple.com/app/id1591439071';
-export const IOS_APP = 'https://play.google.com/store/apps/details?id=com.hoctiengtrung'
+export const IOS_APP = 'https://apps.apple.com/app/id1591439071';
+export const ANDROID_APP = 'https://play.google.com/store/apps/details?id=com.hoctiengtrung'
