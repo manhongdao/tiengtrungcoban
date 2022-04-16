0fb29e32b3983ab528dba05426ce542033c59e0f
manhongdao <0915809219a@gmail.com>
2022-04-16 10:12:38 +0700

    update gap

--- components/pronounce/ThanhMau.tsx
+++ components/pronounce/ThanhMau.tsx
@@ -1,37 +1,37 @@
 import { VolumeUpIcon } from "@heroicons/react/outline";
-import React from "react";
-import THANHMAU_DATA from "../../assets/data/pronounce/ThanhMau";
+import React, { useEffect } from "react";
+import THANHMAU_DATA from "../../public/data/pronounce/ThanhMau";
 import { onClickVolume } from "../../common";
 import Card from "./Card";
 
 function ThanhMau() {
-  return (
-    <div>
-      {THANHMAU_DATA.map((thanhmau) => {
-        return (
-          <div key={thanhmau.header} className="p-2 mb-4">
-            <h3 className="font-medium">{thanhmau.header}</h3>
-            <div className="card-container flex gap-2 flex-wrap">
-              {thanhmau.data.map((items) => {
+    return (
+        <div>
+            {THANHMAU_DATA.map((thanhmau) => {
                 return (
-                  <Card
-                    key={items.id}
-                    id={items.id}
-                    title={items.title}
-                    value={items.value}
-                    iconTitle={
-                      <VolumeUpIcon className="w-6 h-6 text-gray-900 hover:text-gray-700 cursor-pointer" />
-                    }
-                    onClickTitle={onClickVolume}
-                  ></Card>
+                    <div key={thanhmau.header} className="p-2 mb-4">
+                        <h3 className="font-medium">{thanhmau.header}</h3>
+                        <div className="card-container flex gap-4 flex-wrap">
+                            {thanhmau.data.map((items) => {
+                                return (
+                                    <Card
+                                        key={items.id}
+                                        id={items.id}
+                                        title={items.title}
+                                        value={items.value}
+                                        iconTitle={
+                                            <VolumeUpIcon className="w-6 h-6 text-gray-900 hover:text-gray-700 cursor-pointer" />
+                                        }
+                                        onClickTitle={onClickVolume}
+                                    ></Card>
+                                );
+                            })}
+                        </div>
+                    </div>
                 );
-              })}
-            </div>
-          </div>
-        );
-      })}
-    </div>
-  );
+            })}
+        </div>
+    );
 }
 
 export default ThanhMau;
